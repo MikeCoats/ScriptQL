e8ed58bc182fe2d390b14e4dfb8440ce5aa285a6
Mike Coats <i.am@mikecoats.com>
2015-01-23 09:10:57 +0000

    Added a build script that makes a prototype version of the database.
    Prototype here means "obvious" as the ultimate goal will be to fit the
    game in as small a script as possible. That will probably mean reducing
    all table and column names to single characters, so we'll need this
    fully fleshed out version to develop and debug with.

---
+++ build-prototype.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+sqlite3 game.db <<- EOF
+
+    DROP TABLE IF EXISTS pages;
+    CREATE TABLE pages
+    (
+        page_id INT,
+        title VARCHAR
+    );
+
+    DROP TABLE IF EXISTS descriptions;
+    CREATE TABLE descriptions
+    (
+        id INT AUTO_INCREMENT,
+        page INT,
+        part_of_description VARCHAR
+    );
+
+    DROP TABLE IF EXISTS turns;
+    CREATE TABLE turns
+    (
+        page_from INT,
+        page_to INT,
+        description VARCHAR
+    );
+
+    DROP TABLE IF EXISTS messages;
+    CREATE TABLE messages
+    (
+        id INT,
+        message VARCHAR
+    );
+
+    INSERT INTO pages VALUES (1, "Garden");
+    INSERT INTO descriptions(page, part_of_description) VALUES (1, "In the garden.");
+    INSERT INTO descriptions(page, part_of_description) VALUES (1, "This is the garden of a house. The house's front door appears open.");
+    INSERT INTO turns VALUES (1, 2, "Enter the house.");
+
+    INSERT INTO pages VALUES (2, "Front Hall");
+    INSERT INTO descriptions(page, part_of_description) VALUES (2, "In the house.");
+    INSERT INTO descriptions(page, part_of_description) VALUES (2, "The wind catches the door and it clicks behind you.");
+
+    INSERT INTO messages VALUES (1, "You have nowhere else to turn. Your game is over.");
+    INSERT INTO messages VALUES (2, "You can turn to the following pages. Make your choice.");
+
+EOF
+
