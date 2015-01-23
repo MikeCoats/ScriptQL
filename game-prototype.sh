74582c6f44f93d87fdb84160e5a8f931b498beeb
Mike Coats <i.am@mikecoats.com>
2015-01-23 11:32:40 +0000

    Added prompt messages.
    If there are pages to turn to, the game now prompts you, rather than
    just dumping the list of pages on you.
    
    If there are no pages to turn to, it gives you the game over speech.

--- game-prototype.sh
+++ game-prototype.sh
@@ -9,6 +9,12 @@ sqlite3 -list game.db "SELECT part_of_description FROM descriptions WHERE page =
 # Add a space to make it readable.
 echo
 
+# Add a game over message if there are no pages to turn to from here.
+sqlite3 -list game.db "SELECT message FROM messages WHERE id = 1 AND NOT EXISTS (SELECT * FROM turns WHERE page = $1);"
+
+# Add a prompt if there are pages to turn to.
+sqlite3 -list game.db "SELECT message FROM messages WHERE id = 2 AND EXISTS (SELECT * FROM turns WHERE page = $1);"
+
 # Print all of the pages that can be turned to from the selected page.
 sqlite3 -list game.db -separator "  " "SELECT page_to, description FROM turns WHERE page = $1;"
 
